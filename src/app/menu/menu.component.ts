31a72b4fb9e558ba3b035fcfdb9c9485a9b9c2f8
turmz <matthieu.turmo@free.fr>
2017-12-28 15:05:59 +0100

    Detection du scroll pour la top menu bar

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
 // import { setTimeout } from 'timers';
 
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -14,6 +16,7 @@ export class MenuComponent implements OnInit {
   constructor(private rd: Renderer2) { }
 
   ngOnInit() {
+    this.detectScroll(64, 20);
   }
 
   //AFFICHE / DESAFFICHE LE SIDENAV MENU
@@ -21,6 +24,21 @@ export class MenuComponent implements OnInit {
     // on toggle sur l'overlay uniquement s'il est ouvert, sinon on ne fait rien
     if (!$event.srcElement.classList.contains('overlay-side-nav') || this._sideNavOpened) {
       this._sideNavOpened = !this._sideNavOpened;
-    } 
+    }
+  }
+
+  //en fonction du scroll, affiche ou non la top menu bar
+  private detectScroll(offsetUp, offsetDown) {
+    var lastScrollTop = 0;
+    $(window).scroll(function (event) {
+      var scrollLevel = $(this).scrollTop();
+      if (scrollLevel > lastScrollTop + offsetUp) {
+        lastScrollTop = scrollLevel;
+        $('nav.top-menu').css('top', '-64px');
+      } else if (scrollLevel < lastScrollTop - offsetDown) {
+        $('nav.top-menu').css('top', '0');
+        lastScrollTop = scrollLevel;
+      }
+    });
   }
 }
