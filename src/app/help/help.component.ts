71402ca8e9e932fb0750578ebce1ee5976ae9e91
turmz <matthieu.turmo@free.fr>
2017-12-29 16:59:32 +0100

    Page d'aide avec le fullpage, reste toujours à modifier le container en pourcentage pour le slide du navbar

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+
+import 'jquery';
+import 'fullpage.js';
+
+declare var jQuery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-help',
@@ -10,6 +17,31 @@ export class HelpComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.resizeWindow();
+  }
+
+  ngOnDestroy() {
+    //destroying
+    if (typeof $.fn.fullpage.destroy == 'function') {
+      $.fn.fullpage.destroy('all');
+    }
+  }
+
+  private destroyAndReload(next) {
+    if (typeof $.fn.fullpage.destroy == 'function') {
+      $.fn.fullpage.destroy('all');
+      next();
+    }
+  }
+
+  private resizeWindow() {
+    console.log('resize')
+    var self = this;
+    $(document).resize(function (event) {
+      self.destroyAndReload(function () {
+        $('.under-nav').fullpage();
+      });
+    });
   }
 
 }
